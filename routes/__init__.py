1033edc12923fbdca5d5a92f11f1cb1029a297b5
CHARMANTBISSEMOU <charlesbissemou@gmail.com>
2026-02-24 09:16:36 +0100

    Correction routes/__init__.py - importer tous les modules

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,1 +1,13 @@
 # Ce fichier dit à Python que "routes" est un package (un dossier de code)
+
+# Importer tous les modules pour qu'ils soient disponibles
+from . import images
+from . import utilisateurs
+from . import biens
+from . import contrats
+from . import geolocalisation
+from . import medias
+from . import messages
+from . import notifications
+from . import paiements
+from . import webhooks
